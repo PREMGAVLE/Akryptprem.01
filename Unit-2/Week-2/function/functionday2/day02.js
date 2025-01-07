f6dec44c6ec733eaa65024b6bf7b269fac83c648
PREMGAVLE <premgavle51138@gmail.com>
2025-01-07 13:25:33 +0530

    evaluation done

---
+++ Unit-2/Week-2/function/functionday2/day02.js
@@ -0,0 +1,19 @@
+
+
+
+
+// function add(){
+//     result 
+// }
+function prem(a,b){
+return a+b
+}
+function palak(result1){
+return result1*2
+}
+function harshita(result2){
+return result2/4
+}
+function deepak(result3){
+console.log(result3)
+}
